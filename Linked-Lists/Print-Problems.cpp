8768d8b85ff8c521c3de66678ac0df7ac8333ccf
Nikhil Ghosh <nrghosh@wesleyan.edu>
2017-06-15 13:50:23 -0700

    Added ReversePrint

--- Linked-Lists/Print-Problems.cpp
+++ Linked-Lists/Print-Problems.cpp
@@ -25,3 +25,24 @@ void Print(Node *head)
 printf("List traversal done. total num elements = %d\n", curnodenum);
 */
 }
+
+/**************************************************************************************/
+
+/*
+  Print elements of a linked list in reverse order as standard output
+  head pointer could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+void ReversePrint(Node *head)
+{
+  if(head != NULL){
+      ReversePrint(head->next) ; // recursive definition
+      printf("%d\n", head->data) ;
+  }
+  return ;
+}
